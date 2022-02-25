288d2ca75c6163f03cbc94300869d33fc9323b21
GP6 <pcy6004@naver.com>
2022-02-25 15:53:05 +0900

    C#

---
+++ C#/sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("넣어");
+            string name = Console.ReadLine();
+            Console.Write(name);
+        }
+    }
+}
